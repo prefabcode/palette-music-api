1240b1b5235171dd93070ca5be1663045b7fe476
prefabcode <prefabcode@gmail.com>
2026-05-31 17:42:15 -0400

    add entity relationships

--- Api/RegisterDependentServices.cs
+++ Api/RegisterDependentServices.cs
@@ -1,8 +1,17 @@
 // RegisterDependentServices.cs
+
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 public static class RegisterDependentServices
 {
     public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
     {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddDbContext<PaletteDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
